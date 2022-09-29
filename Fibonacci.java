4fe44bd8606a7cd4707fff1da4238acb285763ad
unknown <gori.d@somaiya.edu>
2022-09-30 00:53:26 +0530

    Code added for Fibonacci Sequence

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci{
+
+	public static int fibonacci(int num){
+		if(num==0) return 0;
+		if (num==1 || num==2) return 1;
+		return fibonacci(num-1) + fibonacci(num-2);
+	}
+
+	public static void main(String[] args) {
+		int n = 5;
+		int fib = fibonacci(n);
+		System.out.println(fib);
+	}
+}
